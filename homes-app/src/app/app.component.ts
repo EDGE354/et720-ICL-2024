60980cc2b9d78b34a1eafdbb5c6de20ac34d4730
EC2 Default User <ec2-user@ip-172-31-82-51.ec2.internal>
2024-03-25 05:32:47 +0000

    Stuck at details

--- homes-app/src/app/app.component.ts
+++ homes-app/src/app/app.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { HomeComponent } from './home/home.component';
 
 @Component({
-  selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  selector: 'app-root',
+  template: `
+  <main>
+    <header class="brand-name">
+      <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
+    </header>
+    <section class="content">
+      <app-home></app-home>
+    </section>
+  </main>
+  `,
+  styleUrls: ['./app.component.css'],
+  imports: [HomeComponent]
 })
 export class AppComponent {
-  title = 'homes-app';
+  title = 'homes';
 }
